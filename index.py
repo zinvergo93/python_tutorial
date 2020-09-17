5cb6cabc7c729bc235e9610d44eee16a00ec1930
zinvergo57 <zachary.coding@gmail.com>
2020-09-17 12:20:44 -0500

    added range file

---
+++ index.py
@@ -0,0 +1,20 @@
+starter_sentence = 'The quick brown fox jumped'
+print(starter_sentence[1])
+# will print 'h' from 'The'
+
+# starter_sentence[12] = A ***will not work*** 
+# cannot change values of string once defined
+
+first = starter_sentence[0]
+second = starter_sentence[1]
+third = starter_sentence[2]
+new_sentence = first + second + third                          
+print(new_sentence)
+# will print 'The' because 012 is the first 3 values
+# of the string
+
+first_word = starter_sentence[0:3]
+new_sentence = first_word
+print(new_sentence)
+# will print 'The' [0:3] will read until spot 3
+#  [0:2] will only print 'Th'
