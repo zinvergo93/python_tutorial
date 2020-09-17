1b602d8f16739e78edc859075f981fbfed3f39af
zinvergo57 <zachary.coding@gmail.com>
2020-09-17 11:39:11 -0500

    transfer from replit site

---
+++ apostrophe.py
@@ -0,0 +1,12 @@
+sentence = 'The quick brown fox jumps over the lazy dog'
+
+sentence_two = 'That is my dog\'s bowl'
+
+sentence_three = "That is my dog's bowl"
+
+sentence_four = "Tiffany said, \"That is my dog's bowl\""
+
+print(sentence)
+print(sentence_two)
+print(sentence_three)
+print(sentence_four)
---
+++ practice.py
@@ -0,0 +1,31 @@
+name = 'Zac'
+post_count = 352
+
+print(name)
+print(post_count)
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 0.2
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
+
+meal_completed = True
+total = 100
+tip = total * 1/5
+total = total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altuve'
+
+print(second)
+
+second = 'Correa'
+first = third
+
+print(second)
+print(first)
