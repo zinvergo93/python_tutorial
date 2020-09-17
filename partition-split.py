962fc1f524e943a717458c85af8deec0c73e9477
zinvergo57 <zachary.coding@gmail.com>
2020-09-17 17:33:00 -0500

    add partition-split

---
+++ partition-split.py
@@ -0,0 +1,27 @@
+# heading = "Python: An Introduction"
+
+# header, _, subheader = heading.partition(': ')
+
+# print(header)
+# print(subheader)
+
+# first, second, third = heading.partition(': ')
+
+# print(first)
+# print(second)
+# print(third)
+
+tags = 'python,coding,programming,development'
+
+list_of_tags = tags.split(',')
+
+print(list_of_tags)
+
+list_of_tags = tags.split()
+
+print(list_of_tags)
+
+heading= "Python: An Introduction"
+
+heading_list = heading.split(': ')
+print(heading_list)
