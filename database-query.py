7f1f8450e0c17676604c818597117c65fd045e0d
zinvergo57 <zachary.coding@gmail.com>
2020-09-20 15:32:35 -0500

    update database query notes

--- database-query.py
+++ database-query.py
@@ -5,20 +5,38 @@ Tiffany
 Jordan
 """
 
-users = ['Kristine', 'Tiffany', 'Jordan']
+# users = ['Kristine', 'Tiffany', 'Jordan']
 
+# print(users)
+
+# users.insert(1, 'Anthony')
+
+# print(users)
+
+# users.append('Ian')
+
+# print(users)
+
+# print([users[2]])
+
+# users[4] = 'Brayden'
+
+# print(users)
+
+
+# removal of data
+
+users = ['Kristine', 'Tiffany', 'Jordan', 'Leann']
+# print(users)
+
+# users.remove('Jordan')
+# print(users)
+
+# popped_user = users.pop(0-3) ** use integers for placement
+# print(popped_user)
+# print(users)
+
+del users[]
 print(users)
 
-users.insert(1, 'Anthony')
 
-print(users)
-
-users.append('Ian')
-
-print(users)
-
-print([users[2]])
-
-users[4] = 'Brayden'
-
-print(users)
