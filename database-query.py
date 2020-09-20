c7dadfd5ec7e396d8e8c58a4a93d7388a33aef00
zinvergo57 <zachary.coding@gmail.com>
2020-09-20 14:55:35 -0500

    add database query

---
+++ database-query.py
@@ -0,0 +1,24 @@
+"""
+User Database Query
+Kristine
+Tiffany
+Jordan
+"""
+
+users = ['Kristine', 'Tiffany', 'Jordan']
+
+print(users)
+
+users.insert(1, 'Anthony')
+
+print(users)
+
+users.append('Ian')
+
+print(users)
+
+print([users[2]])
+
+users[4] = 'Brayden'
+
+print(users)
