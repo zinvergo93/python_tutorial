b1994936d05413c1a40d5cd3e4018ab85459452d
zinvergo57 <zachary.coding@gmail.com>
2020-09-24 12:06:43 -0500

    Update all file names

---
+++ python_for_in_2.py
@@ -0,0 +1,48 @@
+# legacy_customers = ['Alice', 'Bob']
+# new_customers = ['Tiffany', 'Kristine']
+
+# for legacy_customer in legacy_customers:
+#     new_customers.append(legacy_customer)
+
+# print(new_customers)
+
+# for new_customer in new_customers:
+#     legacy_customers.append(new_customer)
+
+# print(legacy_customers)
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# result = []
+
+# for number in numbers:
+#     result.append(number + 1)
+
+# print(result)
+
+# prints [2, 3, 4, 5, 6, 7]
+
+
+# *************************************
+
+# num_list = range(1, 11)
+
+# cubed_nums = []
+
+# for num in num_list:
+    # cubed_nums.append(num ** 3)
+
+# print(list(num_list))
+# print(cubed_nums)
+
+
+# num_list = range(1, 11)
+# even_numbers = []
+
+# for num in num_list:
+#     if num % 2 == 0:
+#         even_numbers.append(num)
+
+# even_numbers = [num for num in num_list if num % 2 == 0]
+
+# print(list(num_list))
+# print(even_numbers)
